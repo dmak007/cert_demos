be4a7bdbcc6f44af8f65a26ad8ba88fa70acb1e8
Warwick Stone <uozu.aho@gmail.com>
2019-06-09 12:04:29 +1000

    https server, cleanup, docs

--- 01_simple_http_server.sh
+++ 01_simple_http_server.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# run this with MSYS_NO_PATHCONV=1, because everything has to be difficult
-
-# Serve a single html page over http. Run this then browse to localhost
 
 docker run -p 80:80 -d -v $PWD/static:/usr/share/nginx/html nginx
--- 02_http_port_81_config.sh
+++ 02_http_port_81_config.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# run this with MSYS_NO_PATHCONV=1, because everything has to be difficult
-
-# Demo custom nginx config by running the http server on port 81
 
 docker run -p 81:81 -d \
     -v $PWD/static:/usr/share/nginx/html\
